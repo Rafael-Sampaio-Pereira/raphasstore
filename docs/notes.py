7054ceeea55a000be304b85ecf347f2027ca8406
Rafael Sampaio <rafaelsampaiop@hotmail.com>
2022-08-26 22:50:21 -0300

    First commit. Basic react app. Basic Django API. Docker containers ok.

---
+++ docs/notes.py
@@ -0,0 +1,11 @@
+react_build_for_django = {
+    "build": "rm -rf ../backend/build && react-scripts build && cp -r build ../backend/build && rm -rf build",
+}
+
+generic = {
+    "obs": "Gunicorn can't serve static files"
+}
+
+creation = {
+    "react_ts_project": "yarn create react-app projectname --template typescript "
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import {useQuery } from 'react-query'
+// Components
+import { Drawer } from '@material-ui/core';
+import { LinearProgress } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import { AddShoppingCart } from '@material-ui/icons';
+import Badge from '@material-ui/core';
+
+const App = () => {
+  return (
+    <div className="App">
+      Start
+    </div>
+  );
+}
+
+export default App;
