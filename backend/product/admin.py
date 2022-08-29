e42a0cf82fc0e656ecaaee0d35f7b681e973eef8
Rafael Sampaio <rafaelsampaiop@hotmail.com>
2022-08-29 20:30:51 -0300

    [cod] Adding try except statement

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -21,6 +21,11 @@ class ProductAdmin(admin.ModelAdmin):
         return new_urls + urls
     
     def upload_csv(self, request):
+        
+        errors = ''
+        created_cont = 0
+        index_url = reverse('admin:index')
+        
         if request.method == 'POST':
             csv_file = request.FILES["csv_upload"]
             
@@ -28,49 +33,63 @@ class ProductAdmin(admin.ModelAdmin):
                 messages.warning(request, 'Atenção: O arquivo enviado não é válido!')
                 return HttpResponseRedirect(request.path_info)
             
-            # Getting table data
-            file_data = csv_file.read().decode('utf8')
-            
-            # Split table into lines
-            csv_data = file_data.split('\n')
-            
-            created_cont = 0
-            # for each line in table
-            for index, x in enumerate(csv_data):
-                # skip header line
-                if index == 0:
-                    continue
-                elif index < len(csv_data)-1:
-                    # get line fields, csv must be separetade using ';'
-                    fields = x.split(';')
-                    profit_margin = int(fields[7])
-                    purchase_price = float(str(fields[6]).replace(',','.'))
+            try:
+                
+                # Getting table data
+                file_data = csv_file.read().decode('utf8')
+                
+                # Split table into lines
+                csv_data = file_data.split('\n')
+                
+                # for each line in table
+                for index, x in enumerate(csv_data):
                     
-                    _product, created = Product.objects.update_or_create(
-                        title = fields[0], 
-                        brand  = fields[1], 
-                        category = fields[2],
-                        description = fields[3], 
-                        sku = fields[4],
-                        # calculate final price using profit_margin + purchase_price
-                        price = round(purchase_price + (purchase_price * profit_margin/100), 2))
+                    # skip header line
+                    if index == 0:
+                        continue
                     
-                    if created:
-                        Inventory.objects.update_or_create(
-                            product = _product, 
-                            initial_quantity = fields[5],
-                            remaning_quantity = fields[5],
-                            profit_margin = profit_margin,
-                            purchase_price = purchase_price
-                        )
-                        created_cont += 1
+                    elif index < len(csv_data)-1:
+                        
+                        # get line fields, csv must be separetade using ';'
+                        fields = x.split(';')
+                        profit = int(fields[7])
+                        purchase_price = float(str(fields[6]).replace(',','.'))
+                        
+                        _product, created = Product.objects.update_or_create(
+                            title = fields[0], 
+                            brand  = fields[1], 
+                            category = fields[2],
+                            description = fields[3], 
+                            sku = fields[4],
+                            # calculate final price using profit_margin + purchase_price
+                            price = round(purchase_price + (purchase_price * profit/100), 2))
+                        
+                        if created:
+                            Inventory.objects.update_or_create(
+                                product = _product, 
+                                initial_quantity = fields[5],
+                                remaning_quantity = fields[5],
+                                profit_margin = profit,
+                                purchase_price = purchase_price
+                            )
+                            created_cont += 1
+                            
+            except Exception as e:
+                errors += str(e) + '\n'
             
-            url = reverse('admin:index')
-            messages.success(
-                request, 
-                f'Arquivo .csv processado. {created_cont} produtos cadastrados com sucesso!'
-            )
-            return HttpResponseRedirect(url)   
+            
+            
+            if errors:
+                messages.error(
+                    request, 
+                    f'Arquivo .csv processado. {created_cont} produtos cadastrados. Erros {errors}'
+                )
+            else:
+                messages.success(
+                    request, 
+                    f'Arquivo .csv processado. {created_cont} produtos cadastrados com sucesso!'
+                )
+            return HttpResponseRedirect(index_url)   
 
         form = CsvImportForm()
         data = {"form": form}
