25284547f6c42a9a261d67c22317143cd37cc94a
Rafael Sampaio <rafaelsampaiop@hotmail.com>
2022-08-29 19:39:02 -0300

    [cod] Adds inventory app

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.shortcuts import render
 from django import forms
-from .models import Product
+from .models import Inventory, Product
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -22,15 +22,15 @@ class ProductAdmin(admin.ModelAdmin):
     
     def upload_csv(self, request):
         if request.method == 'POST':
-            print("Enviando csv para o cadastro de produtos...")
-            
             csv_file = request.FILES["csv_upload"]
             
             if not csv_file.name.endswith('.csv'):
                 messages.warning(request, 'Atenção: O arquivo enviado não é válido!')
                 return HttpResponseRedirect(request.path_info)
             
+            # Getting table data
             file_data = csv_file.read().decode('utf8')
+            
             # Split table into lines
             csv_data = file_data.split('\n')
             
@@ -44,20 +44,25 @@ class ProductAdmin(admin.ModelAdmin):
                     # get line fields, csv must be separetade using ';'
                     fields = x.split(';')
                     profit_margin = int(fields[7])
-                    quantity = fields[5], 
                     purchase_price = float(str(fields[6]).replace(',','.'))
                     
-                    created = Product.objects.update_or_create(
+                    _product, created = Product.objects.update_or_create(
                         title = fields[0], 
                         brand  = fields[1], 
                         category = fields[2],
                         description = fields[3], 
                         sku = fields[4],
-                        purchase_price = purchase_price,
                         # calculate final price using profit_margin + purchase_price
                         price = round(purchase_price + (purchase_price * profit_margin/100), 2))
                     
                     if created:
+                        Inventory.objects.update_or_create(
+                            product = _product, 
+                            initial_quantity = fields[5],
+                            remaning_quantity = fields[5],
+                            profit_margin = profit_margin,
+                            purchase_price = purchase_price
+                        )
                         created_cont += 1
             
             url = reverse('admin:index')
@@ -66,11 +71,24 @@ class ProductAdmin(admin.ModelAdmin):
                 f'Arquivo .csv processado. {created_cont} produtos cadastrados com sucesso!'
             )
             return HttpResponseRedirect(url)   
-                
-            
+
         form = CsvImportForm()
         data = {"form": form}
         return render(request, 'admin/csv_upload.html', data)
+    
+
+class InvetoryAdmin(admin.ModelAdmin):
+    read_only_fields = ('created_at', 'updated_at', )
+    list_display = (
+            'product',
+            'initial_quantity',
+            'remaning_quantity',
+            'purchase_price',
+            'profit_margin',
+            'created_at',
+            'updated_at',
+    )
 
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Inventory, InvetoryAdmin)
--- backend/product/models.py
+++ backend/product/models.py
@@ -9,12 +9,46 @@ class Product(models.Model):
     sku = models.CharField(null=True, blank=True, max_length=100)
     image = models.ImageField(null=True, blank=True, upload_to='media')
     price = models.FloatField(null=False, blank=False)
-    purchase_price = models.FloatField(null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     class Meta:
-        ordering = ['title', 'description', 'brand',  'sku', 'category', 'purchase_price', 'price', 'image']
+        ordering = [
+            'title',
+            'description',
+            'brand',
+            'sku',
+            'category',
+            'price',
+            'image'
+        ]
 
     def __str__(self):
         return f'{self.title} {str(self.brand)}'
+    
+
+class Inventory(models.Model):
+    product = models.ForeignKey(
+        "Product",
+        related_name="Product_Inventory",
+        on_delete=models.CASCADE
+    )
+    initial_quantity = models.IntegerField(null=False, blank=False)
+    purchase_price = models.FloatField(null=False, blank=False)
+    remaning_quantity = models.IntegerField(null=False, blank=False)
+    profit_margin = models.IntegerField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = [
+            'product',
+            'initial_quantity',
+            'remaning_quantity',
+            'purchase_price',
+            'profit_margin',
+            'updated_at'
+        ]
+
+    def __str__(self):
+        return f'{self.product.title} {str(self.product.brand)} - {self.remaning_quantity}'
