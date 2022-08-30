0f7b838722d1eb2ee84b007c4c131be07ca29470
Rafael Sampaio <rafaelsampaiop@hotmail.com>
2022-08-30 11:39:54 -0300

    [auth] Adding JWT to the backend project

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 # Configure titles
@@ -41,4 +42,10 @@ urlpatterns += [
          name='swagger-ui'
     ),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+]
+
+# JWTAuthentication
+urlpatterns += [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_view'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh_view'),
 ]
