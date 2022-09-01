df56f387044730c77fcfba836a0586e086908cc9
Rafael Sampaio <rafaelsampaiop@hotmail.com>
2022-09-01 16:38:44 -0300

    [lint] Apply flake8 rules to new files

--- backend/product/forms.py
+++ backend/product/forms.py
@@ -3,4 +3,3 @@ from django import forms
 
 class CsvImportForm(forms.Form):
     csv_upload = forms.FileField(label="Arquivo com Produtos")
-
--- backend/product/urls.py
+++ backend/product/urls.py
@@ -13,10 +13,9 @@ single_actions = {
     "delete": "destroy",
 }
 urlpatterns = [
-    path("", views.ProductViews.as_view(group_actions), 
-         name='get_list_or_create_product'
-    ),
-    re_path(r"^(?P<id>\d+)$", views.ProductViews.as_view(single_actions), 
+    path("", views.ProductViews.as_view(group_actions),
+         name='get_list_or_create_product'),
+    re_path(r"^(?P<id>\d+)$", views.ProductViews.as_view(single_actions),
             name='manage_one_product'),
     path("all/", views.ProductListAPIView.as_view(), name="get_all_products"),
 ]
