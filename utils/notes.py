aab59d4fe3a1ad2ec77a2c9edad3946c60590423
Rafael Sampaio <rafaelsampaiop@hotmail.com>
2022-09-02 01:46:43 -0300

    [clear] Removing olds files

--- utils/notes.py
+++ utils/notes.py
@@ -11,3 +11,16 @@ creation = {
 }
 
 "test": "react-scripts test"
+
+ "devDependencies": {
+    "@typescript-eslint/eslint-plugin": "5.6.0",
+    "@typescript-eslint/parser": "5.6.0",
+    "eslint": "^7.32.0",
+    "eslint-config-airbnb": "19.0.4",
+    "eslint-config-airbnb-base": "15.0.0",
+    "eslint-plugin-import": "^2.25.2",
+    "eslint-plugin-jsx-a11y": "^6.5.1",
+    "eslint-plugin-react": "^7.28.0",
+    "eslint-plugin-react-hooks": "^4.3.0",
+    "typescript": "^4.4.3"
+  }
